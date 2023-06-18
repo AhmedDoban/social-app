b678c1e482060c28adf9f057b7e12dc429c72ece
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-06-18 23:19:48 +0300

    Register

--- src/screens/Guest/Register/Register.jsx
+++ src/screens/Guest/Register/Register.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Register.css";
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
     <div className="Register">
-      <h1>Register</h1>
+      <div className="card">
+        <div className="left">
+          <h1>Social </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/">Login</Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
